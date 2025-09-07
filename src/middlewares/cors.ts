32647eca4db441251f7dda1724875c2fd4e23b30
Junior R <junior31064049@gmail.com>
2025-09-06 22:03:03 -0500

    refactor(cors): update accepted origins and add makeImagesPublic script
    Update CORS accepted origins to only include localhost and production domain.
    Add new script to make all images public in the database.

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,10 +1,6 @@
 import cors from 'cors'
 
-const ACCEPTED_ORIGINS = [
-  'http://localhost:5173',
-  'http://localhost:3000',
-  'https://1rzr331r-5173.use.devtunnels.ms',
-]
+const ACCEPTED_ORIGINS = ['http://localhost:5173', 'https://agrovenca.com']
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
